e8828357f0b6974e70f7b246e1ac65425692ebe1
Choi Jin-won(Kelly Chui) <optioneldar@gmail.com>
2022-05-01 12:53:28 +0900

    Initial Commit

---
+++ NC1 MyAPP/NC1_MyAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_MyAPPApp.swift
+//  NC1 MyAPP
+//
+//  Created by Kelly Chui on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct NC1_MyAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
