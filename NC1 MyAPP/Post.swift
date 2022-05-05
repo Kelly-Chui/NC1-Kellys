32a2e0dfc7ee5d056d6fdfe046d42f95edd8fdf3
Choi Jin-won(Kelly Chui) <optioneldar@gmail.com>
2022-05-06 07:27:00 +0900

    ignore 수정

--- NC1 MyAPP/Post.swift
+++ NC1 MyAPP/Post.swift
@@ -14,12 +14,11 @@ struct PostBlock {
 }
 
 struct Post {
-    //얘네는 옵셔널로
-    var isMyFavorite: Bool = true //어색함
+    var isMyFavorite: Bool = true
     var title: String = "Title"
     var date: String = "Always"
     var coverImage: String = "NoImage"
-    var blockArr: [PostBlock] = [PostBlock(blockType: "image"), PostBlock(text: "안녕하세요 Kelly 입니다."), PostBlock(text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."), PostBlock(blockType: "image", image: "TMI")]
+    var blockArr: [PostBlock] = [PostBlock(blockType: "image"), PostBlock(text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."), PostBlock(blockType: "image", image: "Kelly")]
 }
 
 struct MakeBlock {
